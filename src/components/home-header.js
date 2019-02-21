8d8b9940a6202f812db1a639c61569c113f41fd0
Kyle <kyleplatt2014@gmail.com>
2019-02-20 20:25:33 -0500

    Adding brochure

--- src/components/home-header.js
+++ src/components/home-header.js
@@ -1,8 +1,7 @@
 import React from 'react';
-
 import './style.scss';
-
 import Navbar from './navbar';
+import brochure from '../files/Revolve-Farms-Brochure.pdf';
 
 const HomeHeader = () => (
 	<section id="home-freshcycle" className="hero is-fullheight">
@@ -11,14 +10,21 @@ const HomeHeader = () => (
 			<div id="top-header" className="is-overlay has-text-centered single-spaced">
 				<h1 className="subtitle is-4 has-text-white has-text-weight-light">Revolve Farms</h1>
 				<h2 className="title is-1 has-text-white">freshCycle Greenhouse</h2>
+				<p id="description" className="is-overlay has-text-centered has-text-white">An energy efficient greenhouse designed for the Midwest.</p>
 			</div>
 		</div>
 		<main className="hero-foot center">
-			<div className="center-column">
-				<p id="description" className="is-overlay has-text-centered has-text-white">An energy efficient greenhouse designed for the Midwest.</p>
-				<a href="./lets-talk" id="inquire-btn" className="button is-danger is-inverted is-rounded is-outlined">
+			<div className='bottom-hero-grid'>
+			<div className='center-column'>
+				<a href="./lets-talk" id="inquire-btn" className="button is-danger is-rounded">
 					Inquire
 				</a>
+			</div>
+			<div className='center-column'>
+				<a href={brochure} id="learn-more-btn" className="button is-danger is-inverted is-outlined is-rounded">
+					Learn More
+				</a>
+			</div>
 			</div>
 		</main>
 	</section>
